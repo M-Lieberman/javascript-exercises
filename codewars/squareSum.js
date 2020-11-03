1284a074b8471a4272a39bb041b1b6be87742443
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-11-03 16:28:54 +0000

    squareSum solution 1

--- codewars/squareSum.js
+++ codewars/squareSum.js
@@ -2,7 +2,10 @@
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 function squareSum(numbers) {
-  return numbers.map(n => n * n).reduce( (total, n) => total += n);
+  let r =  numbers.map(n => n * n);
+  return  r.reduce( (total, n) => total += n, 0); // 0 initialises to pretect against an empty array
+   
+  // return numbers.map(n => n * n).reduce( (total, n) => total += n);
 }
 
 
