e7fd5eb8fe8f5f4098be1f532bd097cc61b366dc
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-11-06 15:18:55 +0000

    refactor to arrow functions

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,17 +1,13 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  return nums.filter(function (num) {
-    return num < 1;
-  });
+  return nums.filter((num) => num < 1);
 
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  return names.filter(function (name) {
-    return (name.charAt(0) == char);
-  });
+  return names.filter((name) => name.charAt(0) == char);
 }
 
 function findVerbs(words) {
@@ -24,31 +20,23 @@ function findVerbs(words) {
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  return nums.filter(function (num) {
-    return Number.isInteger(num);
-  });
+  return nums.filter((n) => Number.isInteger(n));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  return users.map(function (user) {
-    return user.data.city.displayName;
-  });
+  return users.map((user) => user.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  return nums.map(function (num) {
-    return Math.round(Math.sqrt(num) * 100) / 100;
-  });
+  return nums.map((num) => Math.round(Math.sqrt(num) * 100) / 100);
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  return sentences.filter(function (sentence) {
-    return sentence.toUpperCase().indexOf(str.toUpperCase()) !== -1;
-  });
+  return sentences.filter((sentence) => sentence.toUpperCase().indexOf(str.toUpperCase()) !== -1);
 }
 
 function getLongestSides(triangles) {
