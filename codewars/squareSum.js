257b7145cf6a1ac5e94c18e6a9e4bd125834a455
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-12-05 16:45:12 +0000

    update squareSum Codewars puzzle to single line

--- codewars/squareSum.js
+++ codewars/squareSum.js
@@ -2,10 +2,10 @@
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 function squareSum(numbers) {
-  let r =  numbers.map(n => n * n);
-  return  r.reduce( (total, n) => total += n, 0); // 0 initialises to pretect against an empty array
+  // let r =  numbers.map(n => n * n);
+  // return  r.reduce( (total, n) => total += n, 0); // 0 initialises to protect against an empty array
    
-  // return numbers.map(n => n * n).reduce( (total, n) => total += n);
+  return numbers.map(n => n * n).reduce( (total, n) => total += n), 0; // the 0 protects against reducing on an empty array
 }
 
 
